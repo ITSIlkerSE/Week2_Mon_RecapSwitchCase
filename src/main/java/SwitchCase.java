6deda55ae8ff6160595d3f2ac1c995ddb7c1ee04
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-05 12:08:32 +0200

    Switch Case practice (unfinished)

---
+++ src/main/java/SwitchCase.java
@@ -0,0 +1,20 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        String subject = "Frau mit Handy";
+
+        switch (subject){
+            case  "Mann mit Handy":
+                System.out.println("Mann mit Telefon");
+                break;
+            case "Opa mit Handy":
+                System.out.println("Opa hat Telefon rausgeholt");
+
+            
+        }
+
+
+    }
+
+
+
+}
